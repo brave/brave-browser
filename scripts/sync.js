17352befa5dedd50fa6fd6a99ff1374ade8d2fe2
bridiver <github@brianjohnson.cc>
2016-11-10 01:14:33 -0700

    build .gclient from npm config

--- scripts/sync.js
+++ scripts/sync.js
@@ -6,17 +6,16 @@ const util = require('../lib/util')
 
 program
   .version(process.env.npm_package_version)
-  .option('--chrome_ref <ref>', 'chrome git ref to checkout')
-  .option('--muon_ref <ref>', 'muon git ref to checkout')
-  .option('--patches_ref <ref>', 'patches git ref to checkout')
-  .option('--node_ref <ref>', 'node git ref to checkout')
   .option('--gclient_file <file>', 'gclient config file location')
   .option('--run_hooks', 'run gclient hooks')
   .option('--run_sync', 'run gclient sync')
   .option('--submodule_sync', 'run submodule sync')
   .option('--init', 'initialize all dependencies')
-  .parse(process.argv)
+config.projectNames.forEach((project) => {
+  program.option('--' + project + '_ref <ref>', project + ' ref to checkout')
+})
 
+program.parse(process.argv)
 config.update(program)
 
 if (program.init || program.submodule_sync) {
@@ -24,29 +23,26 @@ if (program.init || program.submodule_sync) {
 }
 
 if (program.init) {
+  util.buildGClientConfig()
+}
+
+if (program.init) {
   util.gclientSync()
 }
 
-if (program.init || program.chrome_ref) {
-  util.setChromeVersion()
-}
+let updatedVersion = false
 
-if (program.init || program.muon_ref) {
-  util.setMuonVersion()
-}
+config.projectNames.forEach((project) => {
+  if (program.init || program[project + '_ref']) {
+    updatedVersion = true
+    util.setDepVersion(config.projects[project].dir, config.projects[project].ref)
+  }
+})
 
-if (program.init || program.patches_ref) {
-  util.setPatchesVersion()
-}
-
-if (program.init || program.node_ref) {
-  util.setNodeVersion()
-}
-
-if (program.init || program.run_sync) {
+if (updatedVersion || program.init || program.run_sync) {
   util.gclientSync()
 }
 
-if (program.init || program.run_hooks) {
+if (updatedVersion || program.init || program.run_hooks) {
   util.gclientRunhooks()
 }
