c8f1c4f06839b7e162d39d8ea55d7add0e55a476
bridiver <github@brianjohnson.cc>
2016-11-27 21:41:48 -0700

    add upload script

---
+++ scripts/upload.js
@@ -0,0 +1,15 @@
+const program = require('commander');
+const path = require('path')
+const config = require('../lib/config')
+const util = require('../lib/util')
+
+program
+  .version(process.env.npm_package_version)
+  .option('--target_arch <target_arch>', 'target architecture', 'x64')
+  .parse(process.argv)
+
+config.update(program)
+
+const options = config.defaultOptions
+options.cwd = config.projects.muon.dir
+util.run('python', ['script/upload-node-headers.py', '-v', config.electronVersion], options)
