b325176f8c94df67be47aa1bc24fc5b0ede2f8fc
bridiver <github@brianjohnson.cc>
2017-01-26 12:16:54 -0700

    re-organize scripts for cibuild auditors @bbondy @bsclifton

---
+++ scripts/commands.js
@@ -0,0 +1,66 @@
+const program = require('commander');
+const path = require('path')
+const fs = require('fs-extra')
+const config = require('../lib/config')
+const util = require('../lib/util')
+const build = require('../lib/build')
+const createDist = require('../lib/createDist')
+const publish = require('../lib/publish')
+const start = require('../lib/start')
+const updatePatches = require('../lib/updatePatches')
+const upload = require('../lib/upload')
+
+program
+  .version(process.env.npm_package_version)
+
+program
+  .command('build')
+  .option('-C <build_dir>', 'build config (out/Debug, out/Release')
+  .option('--muon', 'build muon')
+  .option('--node', 'build node')
+  .option('--chromedriver', 'build chromedriver')
+  .option('--target_arch <target_arch>', 'target architecture', 'x64')
+  .option('--electron_google_api_key <electron_google_api_key>')
+  .option('--electron_google_api_endpoint <electron_google_api_endpoint>')
+  .option('--no_branding_update', 'don\'t copy BRANDING to the chrome theme dir')
+  .arguments('[build_config]')
+  .action(build)
+
+program
+  .command('create_dist')
+  .option('--target_arch <target_arch>', 'target architecture', 'x64')
+  .action(createDist)
+
+program
+  .command('upload')
+  .option('--target_arch <target_arch>', 'target architecture', 'x64')
+  .action(upload)
+
+program
+  .command('publish')
+  .option('--target_arch <target_arch>', 'target architecture', 'x64')
+  .action(publish)
+
+program
+  .command('start')
+  .option('--v [log_level]', 'set log level to [log_level]', parseInt, '0')
+  .option('--user_data_dir [base_name]', 'set user data directory base name to [base_name]', 'brave-development')
+  .option('--node_env [env]', 'set the node env to [env]', 'development')
+  .arguments('[build_config]')
+  .action(start)
+
+program
+  .command('update_patches')
+  .action(updatePatches)
+
+program
+  .command('cibuild')
+  .option('--target_arch <target_arch>', 'target architecture', 'x64')
+  .action((options) => {
+    build('Release', options)
+    createDist(options)
+    upload(options)
+  })
+
+program
+  .parse(process.argv)
