0460e82e6a93f668a234bec615a48f6cce7d3761
Brian R. Bondy <netzen@gmail.com>
2018-08-15 16:48:44 -0400

    Allow starting Brave in single-process mode
    Example usage:
    npm run start -- --single_process
    
    https://github.com/brave/brave-browser/issues/753

--- scripts/commands.js
+++ scripts/commands.js
@@ -69,6 +69,7 @@ program
   .option('--disable_pdfjs_extension', 'disable loading the PDFJS extension')
   .option('--enable_brave_update', 'enable brave update')
   .option('--channel <target_chanel>', 'target channel to start', /^(beta|dev|nightly|release)$/i, 'release')
+  .option('--single_process', 'use a single process')
   .arguments('[build_config]')
   .action(start)
 
