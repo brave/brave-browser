6ce9797388838caca14669354d27bcc8aab39a87
Mihai Plesa <mplesa@brave.com>
2018-09-21 11:23:44 -0700

    added option to output test results to file

--- scripts/commands.js
+++ scripts/commands.js
@@ -103,6 +103,7 @@ program
   .command('test <suite>')
   .option('--v [log_level]', 'set log level to [log_level]', parseInt, '0')
   .option('--filter <filter>', 'set test filter')
+  .option('--output <output>', 'set test output (results) file path')
   .option('--disable_brave_extension', 'disable loading the Brave extension')
   .option('--single_process', 'uses a single process to run tests to help with debugging')
   .option('--test_launcher_jobs <test_launcher_jobs>', 'Number of jobs to launch')
