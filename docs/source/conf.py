ad2dc7d8a88794a32f324e9eedfe83900bf76b1e
Matt Bacchi <mbacchi@gmail.com>
2018-10-01 14:55:11 -0600

    Add favicon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,7 +63,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = []
+exclude_patterns = ['_build','build',]
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
@@ -99,6 +99,8 @@ html_static_path = ['_static']
 # 'searchbox.html']``.
 #
 # html_sidebars = {}
+
+# html_favicon = "_static/product_logo_32.png"
 
 
 # -- Options for HTMLHelp output ---------------------------------------------
