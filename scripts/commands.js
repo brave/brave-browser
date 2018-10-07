a57d9e1591929e6c2c5e54f9cdd0e20f220b063f
Anthony Tseng <darkdh@gmail.com>
2018-10-06 17:45:56 -0700

    Fix user-data-dir-name switch

--- scripts/commands.js
+++ scripts/commands.js
@@ -62,7 +62,7 @@ program
   .command('start')
   .option('--v [log_level]', 'set log level to [log_level]', parseInt, '0')
   .option('--vmodule [modules]', 'verbose log from specific modules')
-  .option('--user_data_dir_name [base_name]', 'set user data directory base name to [base_name]', 'brave-development')
+  .option('--user_data_dir_name [base_name]', 'set user data directory base name to [base_name]', 'Brave-Browser-Development')
   .option('--no_sandbox', 'disable the sandbox')
   .option('--disable_brave_extension', 'disable loading the Brave extension')
   .option('--disable_brave_rewards_extension', 'disable loading the Brave Rewards extension')
