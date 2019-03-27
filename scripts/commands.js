743d50b5049194cd10f5ba4ec51b70977e68cb6f
Simon Hong <shong@igalia.com>
2019-03-27 11:08:18 +0900

    Make build step sign binaries and generate widevine sig files
    Signing is moved from `create_dist` step because signed binaries are
    needed to generate sig files. And sig files can't be generated by gn
    because of python version of depot_tools.
    So, build step signes and generates sig files by system python and they
    will be used by installer packaging.

--- scripts/commands.js
+++ scripts/commands.js
@@ -39,6 +39,7 @@ program
   .option('--brave_google_api_endpoint <brave_google_api_endpoint>')
   .option('--channel <target_chanel>', 'target channel to build', /^(beta|dev|nightly|release)$/i, 'release')
   .option('--ignore_compile_failure', 'Keep compiling regardless of error')
+  .option('--skip_signing', 'skip signing binaries')
   .arguments('[build_config]')
   .action(build)
 
