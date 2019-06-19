9cd96dd592ae2c17205c095e0fff3fbd8481f58b
Mihai Plesa <mplesa@brave.com>
2019-06-19 14:07:59 +0100

    CI pipeline sync

--- scripts/commands.js
+++ scripts/commands.js
@@ -30,6 +30,7 @@ program
 program
   .command('build')
   .option('-C <build_dir>', 'build config (out/Debug, out/Release')
+  .option('--target_os <target_os>', 'target OS')
   .option('--target_arch <target_arch>', 'target architecture', 'x64')
   .option('--mac_signing_identifier <id>', 'The identifier to use for signing')
   .option('--mac_signing_keychain <keychain>', 'The identifier to use for signing', 'login')
@@ -125,6 +126,8 @@ program
   .option('--disable_brave_extension', 'disable loading the Brave extension')
   .option('--single_process', 'uses a single process to run tests to help with debugging')
   .option('--test_launcher_jobs <test_launcher_jobs>', 'Number of jobs to launch')
+  .option('--target_os <target_os>', 'target OS')
+  .option('--target_arch <target_arch>', 'target architecture', 'x64')
   .arguments('[build_config]')
   .action(test)
 
