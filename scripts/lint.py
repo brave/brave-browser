4fc02b5b2b5c6d662014a9edb5f9553dd994f230
Pranjal Jumde <pranjal.jumde@gmail.com>
2019-10-28 10:41:46 -0700

    Issue 6434: Using vpython for lint

--- scripts/lint.py
+++ scripts/lint.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env vpython
+# Copyright (c) 2019 The Brave Authors. All rights reserved.
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#!/usr/bin/env python
+#!/usr/bin/env vpython
 # Copyright (c) 2013 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
