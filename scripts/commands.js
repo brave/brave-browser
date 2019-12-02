ad170e960e72beaf9efa18e4971802bfbd9c0158
Pete Miller <miller.pete@gmail.com>
2019-12-02 11:50:40 -0800

    Option for 'pull_l10n' script command to only pull for specified resource

--- scripts/commands.js
+++ scripts/commands.js
@@ -118,6 +118,7 @@ program
 program
   .command('pull_l10n')
   .option('--extension <extension>', 'Scope this command to localize a Brave extension such as ethereum-remote-client')
+  .option('--grd_path <grd_path>', `Relative path to match end of full GRD path, e.g: '/generated_resources.grd'.`)
   .action(pullL10n)
 
 program
