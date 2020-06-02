44c6d916fcbfb4efbc2c335b69bd757240e013da
Pete Miller <miller.pete@gmail.com>
2020-06-01 23:09:54 -0700

    sync gets option to ignore chromium update
    Save ~20seconds (50s vs 1m10s)

--- scripts/sync.js
+++ scripts/sync.js
@@ -25,6 +25,7 @@ program
   .option('--all', 'This flag is deprecated and no longer has any effect')
   .option('--force', 'force reset all projects to origin/ref')
   .option('--create', 'create a new branch if needed for [ref]')
+  .option('--no_chromium_update', 'do not check for chromium update')
 
 async function RunCommand () {
   program.parse(process.argv)
@@ -73,7 +74,7 @@ async function RunCommand () {
     }
   }
 
-  util.gclientSync(program.init || program.force, program.init, braveCoreRef)
+  util.gclientSync(program.init || program.force, program.init, braveCoreRef, !program.no_chromium_update)
 
   await util.applyPatches()
 
