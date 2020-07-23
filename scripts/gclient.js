dd19e91333fb7de53feb9dde60b81c5c42321ec7
bridiver <github@brianjohnson.cc>
2020-07-23 11:39:22 -0700

    move commands to brave-core

--- scripts/gclient.js
+++
@@ -1,3 +0,0 @@
-const util = require('../lib/util')
-
-util.runGClient(process.argv.slice(2))
---
+++ scripts/init.js
@@ -0,0 +1,32 @@
+// Copyright (c) 2019 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+
+const DirConfig = require('../lib/dirConfig')
+const fs = require('fs-extra')
+const Log = require('../lib/logging')
+const path = require('path')
+const { spawnSync } = require('child_process')
+const util = require('../lib/util')
+
+Log.progress('Performing initial checkout of brave-core')
+
+const braveCoreRef = util.getProjectVersion('brave-core')
+
+if (!fs.existsSync(path.join(DirConfig.braveCoreDir, '.git'))) {
+  Log.status(`Cloning brave-core [${braveCoreRef}] into ${DirConfig.braveCoreDir}...`)
+  fs.mkdirSync(DirConfig.braveCoreDir)
+  util.runGit(DirConfig.braveCoreDir, ['clone', DirConfig.braveCoreRepo, '.'])
+}
+
+// util.runGit(DirConfig.braveCoreDir, ['reset', '--hard', 'HEAD'])
+// util.runGit(DirConfig.braveCoreDir, ['checkout', braveCoreRef])
+
+util.run('npm', ['install'], { cwd: DirConfig.braveCoreDir })
+util.run('npm', ['run', 'sync'].concat(process.argv.slice(1)), {
+  cwd: DirConfig.braveCoreDir,
+  env: process.env,
+  stdio: 'inherit',
+  shell: true,
+  git_cwd: '.', })
