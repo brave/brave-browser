de7b09cf65e4c19194709e845f9e8d67e5e68b8f
bridiver <github@brianjohnson.cc>
2020-07-23 17:26:57 -0700

    util.getNPMConfig

--- scripts/init.js
+++ scripts/init.js
@@ -17,7 +17,7 @@ const braveCoreRef = util.getProjectVersion('brave-core')
 if (!fs.existsSync(path.join(DirConfig.braveCoreDir, '.git'))) {
   Log.status(`Cloning brave-core [${braveCoreRef}] into ${DirConfig.braveCoreDir}...`)
   fs.mkdirSync(DirConfig.braveCoreDir)
-  util.runGit(DirConfig.braveCoreDir, ['clone', getNPMConfig(['projects', 'brave-core', 'repository', 'url']), '.'])
+  util.runGit(DirConfig.braveCoreDir, ['clone', util.getNPMConfig(['projects', 'brave-core', 'repository', 'url']), '.'])
 }
 
 util.run('npm', ['install'], { cwd: DirConfig.braveCoreDir })
